d17bd260ef2938777e66a51f923b1f8d3a34a9db
Articsteve <johnplutosmithsword@gmail.com>
2019-04-23 15:43:05 -0500

    Chart dates

--- styx/src/main/java/com/lharo/styx/charts/LineChart_AWT.java
+++ styx/src/main/java/com/lharo/styx/charts/LineChart_AWT.java
@@ -19,7 +19,7 @@ public class LineChart_AWT extends ApplicationFrame {
 		JFreeChart lineChart = ChartFactory.createLineChart(
 				chartTitle,
 				"Date","Price",
-				createDataset(),
+				createDataset(flightdates),
 				PlotOrientation.VERTICAL,
 				true,true,false);
 
@@ -28,14 +28,17 @@ public class LineChart_AWT extends ApplicationFrame {
 		setContentPane( chartPanel );
 	}
 
-	private DefaultCategoryDataset createDataset( ) {
+	private DefaultCategoryDataset createDataset(FlightDate[] flightdates ) {
 		DefaultCategoryDataset dataset = new DefaultCategoryDataset( );
-		dataset.addValue( 15 , "schools" , "1970" );
+		/*dataset.addValue( 15 , "schools" , "1970" );
 		dataset.addValue( 30 , "schools" , "1980" );
 		dataset.addValue( 60 , "schools" ,  "1990" );
 		dataset.addValue( 120 , "schools" , "2000" );
 		dataset.addValue( 240 , "schools" , "2010" );
-		dataset.addValue( 300 , "schools" , "2014" );
+		dataset.addValue( 300 , "schools" , "2014" );*/
+		for(FlightDate flight : flightdates) {
+			
+		}
 		return dataset;
 	}
 
